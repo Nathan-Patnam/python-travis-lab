e797d3969a70cb8b8d8315b032d55eaef337c49c
Nathan Patnam <patnamnv@email.wofford.edu>
2019-02-21 23:06:01 -0500

    Added a couple more unit tests to test all of the member variables, next step is to add code coverage and be able to run code coverage on travis ci

--- tests/test_files/dfa_test.py
+++ tests/test_files/dfa_test.py
@@ -2,13 +2,45 @@ import pytest
 from dfa import *
 
 
-def sum(num1, num2):
-    """It returns sum of two numbers"""
-    return num1 + num2
+class TestDFA(object):
+        @pytest.fixture(scope="function")
+        def create_dfa(self,):
+                test_dfa = build_dfa("../dfa_inputs/mock_dfa.txt")
+                return test_dfa
 
+        def test_states(self, create_dfa):
+                states = set(["q2", "q1"])
+                test_dfa_states = create_dfa.states
+                assert states == test_dfa_states
+
+        def test_alphabet(self, create_dfa):
+                alphabet = set(["a", "b"])
+                test_dfa_alphabet = create_dfa.alphabet
+                assert alphabet == test_dfa_alphabet
+
+        def test_start_state(self, create_dfa):
+                start_state = "q1"
+                test_dfa_state_state = create_dfa.start_state
+                assert start_state == test_dfa_state_state
+
+        def test_accept_state(self, create_dfa):
+                accept_states = set(["q2"])
+                test_dfa_accept_states = create_dfa.accept_states
+                assert accept_states == test_dfa_accept_states
+
+        def test_transitions(self, create_dfa):
+                transitions = {"q1": {"a": "q2", "b": "q1"},
+                            "q2": {"a": "q2", "b": "q2"}}
+                test_dfa_transitions = create_dfa.transitions
+                assert transitions == test_dfa_transitions
 
 
 """
+
+def sum(num1, num2):
+It returns sum of two numbers
+    return num1 + num2
+
 paramaterize tests -> test multiple sets of inputs at once
 def get_sum_test_data():
         return [(3,5,8), (-2,-2,-4), (-1,5,4), (3,-5,-2), (0,5,5)]
@@ -16,10 +48,6 @@ def get_sum_test_data():
 @pytest.mark.parametrize('num1, num2, expected',get_sum_test_data())
 def test_sum(num1, num2, expected):
         assert sum(num1, num2) == expected
-
-
-"""
-
 
 @pytest.fixture
 def get_sum_test_data():
@@ -32,25 +60,4 @@ def test_sum(get_sum_test_data):
                 expected = data[2]
                 assert sum(num1, num2) == expected
 
-
-# class TestDFA(object):
-#     def __init__(self):
-
-
-#     @classmethod
-#     def setup_class(cls):
-#         test_dfa = build_dfa("./dfa_inputs/mock_dfa.txt")
-#         print(test_dfa.states())
-
-#     def setup_method(self, method):
-#         """ setup any state tied to the execution of the given method in a
-#         class.  setup_method is invoked for every test method of a class.
-#         """
-    
-
-
-
-
-
-
-
+"""
